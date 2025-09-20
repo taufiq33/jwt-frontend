ef5b3e5da0ec0cd0f3546262df4b0328c2c0f7c7
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-09-20 09:28:30 +0700

    feat: organizing navbar item based on user auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Form, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
+  const accessToken = useSelector((state) => state.auth.accessToken);
+
   return (
     <ul className="flex gap-2 p-2 ">
       <li>
@@ -8,28 +11,40 @@ export default function Navbar() {
           Home
         </NavLink>
       </li>
-      <li>
-        <NavLink className="border-b-1 text-blue-500" to="/dashboard">
-          Dashboard
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className="border-b-1 text-green-500" to="/login">
-          Login
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className="border-b-1 text-green-500" to="/register">
-          Register
-        </NavLink>
-      </li>
-      <li>
-        <Form action="/logout" method="post">
-          <button className="border-b-1 text-red-500" type="submit">
-            Logout
-          </button>
-        </Form>
-      </li>
+
+      {accessToken && (
+        <li>
+          <NavLink className="border-b-1 text-blue-500" to="/dashboard">
+            Dashboard
+          </NavLink>
+        </li>
+      )}
+      {!accessToken && (
+        <>
+          <li>
+            <NavLink className="border-b-1 text-green-500" to="/login">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="border-b-1 text-green-500" to="/register">
+              Register
+            </NavLink>
+          </li>{" "}
+        </>
+      )}
+      {accessToken && (
+        <li>
+          <Form action="/logout" method="post">
+            <button
+              className="cursorpointer border-b-1 text-red-500"
+              type="submit"
+            >
+              Logout
+            </button>
+          </Form>
+        </li>
+      )}
     </ul>
   );
 }
